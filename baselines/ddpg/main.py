33015f86885851ed80ea8461ea0e7a8d14efa8bb
pifouuu <pierre00fournier@gmail.com>
2017-11-08 13:45:46 +0100

    Back to main corresponding to origin/master; only diff is the addition of memories and env_wrapper choice

--- baselines/ddpg/main.py
+++ baselines/ddpg/main.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 import os
 import logging
 from baselines import logger, bench
@@ -14,22 +15,21 @@ from baselines.ddpg.noise import *
 
 import gym
 import tensorflow as tf
+from mpi4py import MPI
 
-def run(env_id, seed, noise_type, memory_type, env_wrapper_type, norm_type, evaluation, **kwargs):
-
+def run(env_id, seed, noise_type, memory_type, env_wrapper_type, layer_norm, evaluation, **kwargs):
+    rank = MPI.COMM_WORLD.Get_rank()
+    if rank != 0: logger.set_level(logger.DISABLED)
     # Create envs.
     env = gym.make(env_id)
-    filename = logger.get_dir() and os.path.join(logger.get_dir(), "monitor.json")
-    env = bench.Monitor(env, filename, allow_early_resets=True)
+    env = bench.Monitor(env, logger.get_dir() and os.path.join(logger.get_dir(), "%i.monitor.json"%rank))
     gym.logger.setLevel(logging.WARN)
     logger.configure(dir='/home/pierre/PycharmProjects/baselines/baselines/ddpg/log',
                      format_strs=['stdout', 'json', 'tensorboard'])
 
-    # if evaluation
-    if evaluation :
+    if evaluation and rank==0:
         eval_env = gym.make(env_id)
-        eval_env = bench.Monitor(eval_env, os.path.join(logger.get_dir(), 'gym_eval'),
-                                 allow_early_resets=True)
+        eval_env = bench.Monitor(eval_env, os.path.join(logger.get_dir(), 'gym_eval'))
     else:
         eval_env = None
 
@@ -71,30 +71,22 @@ def run(env_id, seed, noise_type, memory_type, env_wrapper_type, norm_type, eval
     else:
         raise RuntimeError('unknown memory type "{}"'.format(memory_type))
 
-    # Parse Normalization type
-    layer_norm = False
-    batch_norm = False
-    if norm_type=='layer':
-        layer_norm = True
-    elif norm_type=='batch':
-        batch_norm = True
-    elif norm_type=='none':
-        pass
-    else:
-        raise RuntimeError('unknown norm type "{}"'.format(norm_type))
-
     # Critic and actor
-    critic = Critic(layer_norm=layer_norm, batch_norm=batch_norm)
-    actor = Actor(nb_actions, layer_norm=layer_norm, batch_norm=batch_norm)
+    critic = Critic(layer_norm=layer_norm)
+    actor = Actor(nb_actions, layer_norm=layer_norm)
 
     # Seed everything to make things reproducible.
+    seed = seed + 1000000 * rank
+    logger.info('rank {}: seed={}, logdir={}'.format(rank, seed, logger.get_dir()))
     tf.reset_default_graph()
     set_global_seeds(seed)
     env.seed(seed)
     if eval_env is not None:
         eval_env.seed(seed)
 
-    # Training
+    # Disable logging for rank != 0 to avoid noise.
+    if rank == 0:
+        start_time = time.time()
     training.train(env=env, eval_env=eval_env, param_noise=param_noise,
         action_noise=action_noise, actor=actor, critic=critic, memory=memory, **kwargs)
 
@@ -102,17 +94,21 @@ def run(env_id, seed, noise_type, memory_type, env_wrapper_type, norm_type, eval
     env.close()
     if eval_env is not None:
         eval_env.close()
+    if rank == 0:
+        logger.info('total runtime: {}s'.format(time.time() - start_time))
 
 
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
     parser.add_argument('--env-id', type=str, default='MountainCarContinuous-v0')
     boolean_flag(parser, 'render-eval', default=False)
+    boolean_flag(parser, 'layer-norm', default=True)
     boolean_flag(parser, 'render', default=False)
     boolean_flag(parser, 'normalize-returns', default=False)
-    boolean_flag(parser, 'normalize-observations', default=False)
-    parser.add_argument('--seed', help='RNG seed', type=int, default=4)
-    parser.add_argument('--critic-l2-reg', type=float, default=0.01)
+    boolean_flag(parser, 'normalize-observations', default=True)
+    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
+    parser.add_argument('--critic-l2-reg', type=float, default=1e-2)
     parser.add_argument('--batch-size', type=int, default=64)  # per MPI worker
     parser.add_argument('--actor-lr', type=float, default=1e-4)
     parser.add_argument('--critic-lr', type=float, default=1e-3)
@@ -120,15 +116,14 @@ def parse_args():
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--reward-scale', type=float, default=1.)
     parser.add_argument('--clip-norm', type=float, default=None)
-    parser.add_argument('--nb-episodes', type=int, default=10000)  # with default settings, perform 1M steps total
+    parser.add_argument('--nb-epochs', type=int, default=500)  # with default settings, perform 1M steps total
+    parser.add_argument('--nb-epoch-cycles', type=int, default=20)
     parser.add_argument('--nb-train-steps', type=int, default=50)  # per epoch cycle and MPI worker
-    parser.add_argument('--train-freq', type=int, default=1)
     parser.add_argument('--nb-eval-steps', type=int, default=1000)  # per epoch cycle and MPI worker
     parser.add_argument('--nb-rollout-steps', type=int, default=1000)  # per epoch cycle and MPI worker
     parser.add_argument('--noise-type', type=str, default='ou_0.3')  # choices are adaptive-param_xx, ou_xx, normal_xx, none
     parser.add_argument('--memory-type', type=str, default='standard') # choices are standard, her, no_reward
     parser.add_argument('--env-wrapper-type', type=str, default='standard') # choices are standard and goal
-    parser.add_argument('--norm-type', type=str, default='none')  # Choices are batch, layer or none
     boolean_flag(parser, 'evaluation', default=False)
     return vars(parser.parse_args())
 
